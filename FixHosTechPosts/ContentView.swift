ffe1e19c9ad6df50b88035d493f6029685d0c3b7
jigme1968 <jigme1968@gmail.com>
2020-07-23 21:23:33 +0800

    Initial Commit

---
+++ FixHosTechPosts/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FixHosTechPosts
+//
+//  Created by jk on 2020/7/23.
+//  Copyright © 2020 ssl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
