102f758d80c24ccfe341430dee8993711260403d
jigme1968 <jigme1968@gmail.com>
2020-08-11 17:18:01 +0800

    modified

--- FixHosTechPosts/Views/OperationGroup.swift
+++ FixHosTechPosts/Views/OperationGroup.swift
@@ -13,26 +13,28 @@ struct OperationGroup: View {
     var elementIndex: Int
     
     var body: some View {
-        //Text("操作组")
-        VStack(alignment:.leading) {
-            VStack {
-                ForEach(userData.operatorUnitsAt(self.elementIndex)) { unit in
-                    VStack(alignment:.leading) {
-                        Text("\(unit.操作组名称)").font(.body).foregroundColor(.purple)
-                        OperateUnits(elementIndex:self.elementIndex,operatorUnit: unit)
-                        Spacer()
-                        Divider()
-                        DetailAddItem(elementIndex: self.elementIndex, 操作组名称: unit.操作组名称, techsPerItem: 0, nursesPerItem: 0, doctorsPerItem: 0, curerPerItem: 0, securitryPerItem: 0)
-                        Divider()
+        ScrollView {
+            //Text("操作组")
+            VStack(alignment:.leading) {
+                VStack {
+                    ForEach(userData.operatorUnitsAt(self.elementIndex)) { unit in
+                        VStack(alignment:.leading) {
+                            Text("\(unit.操作组名称)").font(.body).foregroundColor(.purple)
+                            OperateUnits(elementIndex:self.elementIndex,operatorUnit: unit)
+                            Spacer()
+                            Divider()
+                            DetailAddItem(elementIndex: self.elementIndex, 操作组名称: unit.操作组名称, techsPerItem: 0, nursesPerItem: 0, doctorsPerItem: 0, curerPerItem: 0, securitryPerItem: 0)
+                            Divider()
+                        }
+                        
                     }
-                
                 }
+                Spacer()
+                Divider()
+                Text("根据诊疗操作类型和项目分组,没有机器,或人手与机器数量无线性关系").font(.body).foregroundColor(.red)
+                DetailAddOperationGroup(elementIndex: elementIndex)
+                
             }
-            Spacer()
-            Divider()
-            Text("根据诊疗操作类型和项目分组,没有机器,或人手与机器数量无线性关系").font(.body).foregroundColor(.red)
-            DetailAddOperationGroup(elementIndex: elementIndex)
-
         }
     }
 }
